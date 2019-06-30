51e43aa64e8e200e2364ca3db70a23990956dbcb
jeremyb61 <jeremyb61@live.com>
2019-06-30 16:03:56 -0700

    Final touch ups

--- src/database/dbconnection.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-
-module.exports = new Sequelize('onpointaccademy', 'salbonico@onpointaccademy', 'Steveandjer22', {
-    host: 'onpointaccademy.mysql.database.azure.com',
-    dialect: 'mysql'
-});
-
